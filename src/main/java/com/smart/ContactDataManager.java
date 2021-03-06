20854daac19e63396df7a72b0fed85a91219379c
Ajaz <Ajaz@md>
2021-03-06 15:26:17 +0530

    CD-122 retest maven clean test

--- src/main/java/com/smart/ContactDataManager.java
+++ src/main/java/com/smart/ContactDataManager.java
@@ -3,8 +3,9 @@ package com.smart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class ContactDataManager {
+public class ContactDataManager{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactDataManager.class, args);
